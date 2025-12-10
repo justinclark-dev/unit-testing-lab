28288ae2d4d9f93fbd8357a2cf0756fd7f4c2b79
justinclark-dev <justinclark.dev@gmail.com>
2025-12-09 21:20:38 -0800

    adds some basic tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,57 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleInputChange = (event) => {
+    const createNewTodo = (event.target.value);
+    setNewTodo(createNewTodo);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { id: todos.length + 1, text: newTodo }]);
+    setNewTodo('');
+  };
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
 
   return (
     <>
       <h1>TODO App</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          id="new-todo" 
+          value={newTodo}
+          onChange={handleInputChange}
+          placeholder='Enter TODO'
+        />
+        <button type="submit">
+          Add
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}
+          
+          <button
+            onClick={() => handleDelete(todo.id)}
+          >
+            Delete
+          </button>
+          
+          </li>
+        ))}
+      </ul>
     </>
   )
-}
+};
 
-export default App
+export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from './App';
 import { expect, test } from "vitest";
 
@@ -6,4 +6,34 @@ test('renders an h1 tag', () => {
   const app = render(<App />);
   const heading = app.getByRole('heading');
   expect(heading).toHaveTextContent('TODO App');
+});
+
+test('changes input', () => {
+  const app = render(<App />);
+  const textbox = app.getByRole('textbox');
+  fireEvent.change(textbox, { target: { value: 'do something' } });
+  expect(textbox.value).toBe('do something');
+});
+
+test('adds a new todo', () => {
+  const app = render(<App />);
+  const textbox = app.getByRole('textbox');
+  const button = app.getByRole('button');
+  fireEvent.change(textbox, { target: { value: 'do another'} })
+  fireEvent.click(button);
+  const list = app.getByRole('list');
+  expect(list).toHaveTextContent('do another');
+});
+
+test('deletes a todo', () => {
+  const app = render(<App />);
+  const textbox = app.getByRole('textbox');
+  const button = app.getByRole('button');
+  fireEvent.change(textbox, { target: { value: 'to delete'} })
+  fireEvent.click(button);
+  const list = app.getByRole('list');
+  expect(list).toHaveTextContent('to delete');
+  const deleteBtn = app.getByRole('button', { name: 'Delete' });
+  fireEvent.click(deleteBtn);
+  expect(list).not.toHaveTextContent('to delete')
 });
